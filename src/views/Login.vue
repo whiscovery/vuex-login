1295bae68c070a9bdc1ccceae60b73491f012bcb
whiscovery <whiscovery@gmail.com>
2020-09-29 22:37:59 +0900

    1

---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container fill-height style="max-width:450px">
+    <v-layout align-center row wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-toolbar flat>
+            <v-toolbar-title>Login</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <div class="pa-3">
+            <v-text-field v-model="email" label="Email"> </v-text-field>
+            <v-text-field v-model="password" type="password" label="password">
+            </v-text-field>
+          </div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      password: null
+    }
+  }
+}
+</script>
